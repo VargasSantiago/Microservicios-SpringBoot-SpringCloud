70a25a7f5b4185cc45caed5be3d3ba41b2a28ce0
VargasSantiago <vargas.santiago2002@gmail.com>
2023-04-14 12:45:27 -0300

    Feing

--- shopping-service/src/main/java/academy/digitallab/store/shopping/entity/Invoice.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/entity/Invoice.java
@@ -1,5 +1,6 @@
 package academy.digitallab.store.shopping.entity;
 
+import academy.digitallab.store.shopping.model.Customer;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
@@ -14,7 +15,6 @@ import java.util.List;
 @Entity
 @Table(name = "tlb_invoices")
 public class Invoice {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,6 +41,9 @@ public class Invoice {
     private List<InvoiceItem> items;
 
     private String state;
+
+    @Transient
+    private Customer customer;
 
     public Invoice(){
         items = new ArrayList<>();
